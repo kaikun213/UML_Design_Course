88abb897253666cc3e36d8bd8a3aaf705fb601f4
kaikun213 <kaktus.w.h@gmx.de>
2016-09-22 15:12:51 +0200

    Test Main function

---
+++ src/TestApplication.java
@@ -0,0 +1,24 @@
+import model.Member;
+import model.MemberList;
+
+public class TestApplication {
+
+	public static void main(String[] args) {
+		Member md = new Member();
+		md.setId("1");
+		md.setName("Tester");
+		md.setPersonal_number("12312");
+		
+		Member md2 = new Member();
+		md2.setId("2");
+		md2.setName("Peter");
+		md2.setPersonal_number("3212");
+		
+		MemberList md_list = new MemberList();
+		md_list.addMember(md);
+		md_list.addMember(md2);
+		
+		dao.MembersDAO.jaxbObjectToXML(md_list);
+	}
+
+}
