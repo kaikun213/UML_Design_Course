cb701e67d0616d7b6bf5788d8b06a774882795a8
kaikun213 <kaktus.w.h@gmx.de>
2016-10-01 15:36:19 +0200

    changed some responsibilities to admin class + search -> more methids needed

--- src/view/IView.java
+++ src/view/IView.java
@@ -2,6 +2,7 @@ package view;
 
 import java.util.Iterator;
 
+import controller.Admin.ValidationType;
 import model.Boat;
 import model.Member;
 
@@ -9,13 +10,13 @@ public interface IView {
 	// show, select, edit, delete, create
 	
 	void showWelcomeMessage();
-	int selectInstruction();						// 1)create, 2)delete or 3) edit member/boats 4) list members
+	int selectInstruction();						// 1)create, 2)delete or 3) edit member/boats 4) list members 5) search
 	int selectListType();						// 1) compact , 2) verbose
 	void showCompactList(Iterator<Member> m_it); // “Compact List”; name, member id and number of boats
 	void showVerboseList(Iterator<Member> m_it); // “Verbose List”; name, personal number, member id and boats with boat information
+	
 	int selectMember();							// returns unique member-ID as String
 	void showMember(int id, String name, String personal_number, Iterator<Boat> b_it);
-	
 	Member createMember();						// gets unique ID from system
 	boolean deleteMemberConfirmation(int id);	// true = deletion , id = unique id of selected member
 	int selectMemberEdit();						// 1) edit name, 2) edit personal-number, 3) edit boats or 4) to quit editing
@@ -28,10 +29,15 @@ public interface IView {
 	int selectBoat();							// returns unique boat-id
 	Boat createBoat();
 	Boat editBoat(Boat b);							// edit boats type and length, not ID!
-	boolean deleteBoatConfirmation(int id);	// true = deletion , id = member-id (owner of boat)
-
-
+	boolean deleteBoatConfirmation(int id);		// true = deletion , id = member-id (owner of boat)
 	
+	int selectSearch();							// 1) name 2) age 3) birth-month 4) boat-type
+	String getSearchParam(ValidationType t);
+	int selectMonth();							// selection from constant enum Calendar.Month
+	int selectBoatsType();						// selection from constant enum Boat.BoatType
+	
+	void showSuccessMessage(String s);
+	void showErrorMessage(String s);
 	
 	boolean wantsToManage();
 
